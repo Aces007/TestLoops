a414c8a93154cb17a38034ad73dba6c2b18ce6b3
Ace Lawrence <clavanoace@gmail.com>
2021-11-28 18:10:45 -0800

    program 1 modification in progress

--- Word, Vowel, and Consonant Counter.py
+++ Word, Vowel, and Consonant Counter.py
@@ -3,20 +3,24 @@ import string
 AskingForStatement = input("Enter your statement: ")
 print (AskingForStatement)
 
+
 words = 0
 vowels = 0
 consonants = 0
+
+
 split_AskingForStatement = AskingForStatement.split()
-
 IlangSalita = len(split_AskingForStatement)
-
 print("The statement contain:{}".format (IlangSalita) + " words")
 
+VowelsCapital = ["A", "E", "I", "O", "U"]
+VowelsLowercase = ['a', 'e', 'i', 'o', 'u']
 
-for statement in AskingForStatement:
-    if statement == 'a' or statement == 'e' or statement == 'i' or statement == 'o' or statement == 'u':
+
+for words in AskingForStatement:
+    if words == 'a' or words == 'e' or words == 'i' or words == 'o' or words == 'u':
         vowels = vowels + 1
-    elif statement == 'A' or statement == 'B' or statement == 'I' or statement == 'O' or statement == 'U':
+    elif words == 'A' or words == 'B' or words == 'I' or words == 'O' or words == 'U':
         vowels = vowels + 1
     else:
         consonants = consonants + 1        
