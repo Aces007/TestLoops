975d9d1e54719649e1a85cfe71717c9c381eb4b0
Ace Lawrence <clavanoace@gmail.com>
2021-11-29 16:19:45 -0800

    added comments and instructions for each program

--- Password Validator.py
+++ Password Validator.py
@@ -1,17 +1,23 @@
+# The importing of this regular expression specifies a certain expression that matches with the given conditions in your program.
 import re
 
+# To begin with, this first line will ask you for a new password, it will have its criteria prepared on the event you are typing your combinations.
 CreateNewPassword = input("Enter a new password: ")
+# This line record the chances you have for verifying your entered password. 
 Chances = 8
+# Now these 4 lines here records the characters your password contain including the capital letters, numbers, and sepcial characters.  
 CharactersPo = (len(CreateNewPassword))
 CapitalLettersNaman = len(re.findall (r'[A-Z]', CreateNewPassword))
 NumbersAsWell = sum(characters.isdigit() for characters in CreateNewPassword)
 LastlySpecialCharacters = len(CreateNewPassword) - len(re.findall ('[\w]', CreateNewPassword))
 
+# Then these print functions presents the quantity for each variable.
 print (CharactersPo)
 print (CapitalLettersNaman)
 print(NumbersAsWell)
 print (LastlySpecialCharacters)
 
+# Now these 4 functions are responsible for checking your passwords validity and if you have matched with the criteria on the algorithm.
 def Characters():
     if CharactersPo >= 15:
         print ("Now let's check the numbers, special characters and letters incorporated in your combination")
@@ -20,6 +26,7 @@ def Characters():
         EnterAnotherOne  = input("Enter a new password: ")
         return EnterAnotherOne
 
+# This is for evaluating your passwords content for letters specifically Uppercase ones.
 def CapitalLetters():
     if CapitalLettersNaman >= 1:
         print ("Okay, now for the Numbers")
@@ -28,6 +35,7 @@ def CapitalLetters():
         EnterOneAgain = input("Enter a new password: ")
         return EnterOneAgain
 
+# Then, this is for checking if the password has numbers. 
 def Numbers():
     if NumbersAsWell >= 1:
         print ("Okay, lastly the Special Characters")
@@ -36,6 +44,8 @@ def Numbers():
         EnterItAgainPlease = input("Enter a new password: ")
         return EnterItAgainPlease
 
+# Lastly, this is for checking if you have special characters included to make your password stronger, if not met the program will tell you 
+# that you have not satisifed the conditions and will prompt you to enter a much stronger password.
 def SpecialCharacters():
     if LastlySpecialCharacters >= 1:
         print ("You have met all the criteria")
@@ -45,12 +55,13 @@ def SpecialCharacters():
         EnterAStrongerPassword = input("Enter a new password: ")
         return EnterAStrongerPassword
 
+# These lines will call the functions. 
 EnterAnotherOne = Characters()
 EnterOneAgain = CapitalLetters()
 EnterItAgainPlease = Numbers()
 EnterAStrongerPassword = SpecialCharacters()
 
-
+# Then this will initialize if the requirements are met. This will now evaluate your password if they are the same and valid. 
 for statement in range(8, 0 , -1):
     MagkaparehasBa = input("Enter Password Again: ")
     if CreateNewPassword == MagkaparehasBa:
@@ -66,7 +77,9 @@ for statement in range(8, 0 , -1):
     else:
         print ("Incorrect password, Try again!")  
         Chances = Chances - 1
-
+# If not you will be deducted attempts for evaluating your password until you have ran out and you are locked out of your account.
+# But if you have evaluated your password successfully, you are granted access to your account and will be presented with the chances you have 
+# left for typing your passwords multiple times.
 if statement == 1:
     print ("Your account is locked temporarily")
 else:
--- Word, Vowel, and Consonant Counter.py
+++ Word, Vowel, and Consonant Counter.py
@@ -1,14 +1,15 @@
+# This will prompt you "the user" for the statement you want to analyze for words, vowels, and consonants.
 Statement = input("Please enter your statement: ")
 
-
+# Now these two variables record the number of the vowels and consonants that is presented by the print functions below.  
 vowels = 0
 consonants = 0
 
-
+# These variables now are responsible for defining the vowels below and what they contain.   
 VowelsUppercase = ("A", "E", "I", "O", "U")
 VowelsLowercase = ("a", "e", "i", "o", "u")
 
-
+# Now this for loop is responsbile for initializing the count for the vowels and consonants.
 for elements in Statement:
     if elements in VowelsUppercase:
         vowels = vowels + 1 
@@ -17,11 +18,11 @@ for elements in Statement:
     else:
         consonants = consonants + 1
 
-
+# Independently, these two lines count the words found on the sentences stated above. They are presented by the print function below.
 split_Statement = Statement.split()
 IlangSalita = len(split_Statement)
 
-
+# Lastly, these print funtions are responsible for presenting you the number of the words including the vowels and consonants each words contain.
 print(("The statement contain:{}".format (IlangSalita) + " words"))
 print (f"{vowels}" + " vowels")
 print (f"{consonants}" + " consonants")
